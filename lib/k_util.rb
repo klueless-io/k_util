e6879397acd367a4307c4b95558c3dffc6334e2f
klueless-io <david@ideasmen.com.au>
2021-03-30 21:11:43 +1100

    first commit

---
+++ lib/k_util.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "k_util/version"
+
+module KUtil
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/k_util/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KUtil
+  VERSION = "0.1.0"
+end
