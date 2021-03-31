69f4f0ac0d97abbd4290295788d9d5ece67193d0
klueless-io <david@ideasmen.com.au>
2021-03-31 11:27:57 +1100

    add data helpers and documentation

--- STORIES.md
+++ STORIES.md
@@ -6,27 +6,22 @@ As a Developer, I need simple utility helpers, to solve cross cutting issues and
 
 ## Development radar
 
-### Stories next on list
+## Stories and tasks
 
-As a Developer, I can DO_SOMETHING, so that I QUALITY_OF_LIFE
+### Stories - completed
 
-- Subtask
+As a Developer, I need simple utility helpers, to solve cross cutting issues
 
-### Tasks next on list
+- Console helpers
+- Data helpers
+- File helpers
+
+### Tasks - completed
 
 Setup RubyGems and RubyDoc
 
 - Build and deploy gem to [rubygems.org](https://rubygems.org/gems/k_util)
 - Attach documentation to [rubydoc.info](https://rubydoc.info/github/to-do-/k_util/master)
-
-Setup GitHub Action (test and lint)
-
-- Setup Rspec action
-- Setup RuboCop action
-
-## Stories and tasks
-
-### Tasks - completed
 
 Setup project management, requirement and SCRUM documents
 
@@ -34,6 +29,11 @@ Setup project management, requirement and SCRUM documents
 - Setup user stories and tasks
 - Setup a project backlog
 - Setup an examples/usage document
+
+Setup GitHub Action (test and lint)
+
+- Setup Rspec action
+- Setup RuboCop action
 
 Setup new Ruby GEM
 
--- spec/k_util/data_helper_spec.rb
+++ spec/k_util/data_helper_spec.rb
@@ -1,18 +1,36 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'json'
+
+ThunderBirds = Struct.new(:action)
 
 RSpec.describe KUtil::DataHelper do
+  # it 'sample' do
+  #   virgil = OpenStruct.new(name: 'Virgil Tracy', age: 73, thunder_bird: ThunderBirds.new(:are_grounded))
+  #   penny = OpenStruct.new(name: 'Lady Penelope', age: 69, thunder_bird: ThunderBirds.new(:are_go))
+
+  #   data = {
+  #     key1: 'David',
+  #     key2: 333,
+  #     key3: ThunderBirds.new(:are_go),
+  #     people: [virgil, penny]
+  #   }
+
+  #   data_open = KUtil.data.to_open_struct(data)
+  #   data_hash = KUtil.data.to_hash(data_open)
+
+  #   puts JSON.pretty_generate(data_hash)
+  # end
+
   describe 'module helper' do
     subject { KUtil.data }
 
     it { is_expected.not_to be_nil }
-
-    # it { expect(subject.build_unique_key('key', 'type', 'namespace')).to eq('namespace_key_type') }
   end
 
-  describe '#to_struct' do
-    subject { described_class.to_struct(data) }
+  describe '#to_open_struct' do
+    subject { described_class.to_open_struct(data) }
 
     # it { expect { subject }.to raise_error KError }
     context 'when simple hash' do
@@ -20,6 +38,7 @@ RSpec.describe KUtil::DataHelper do
 
       it { is_expected.to have_attributes(key1: 'David', key2: 333) }
     end
+
     context 'when array of hash' do
       let(:data) { { items: [{ id: 1, name: 'Item1' }, { id: 2, name: 'Item2' }] } }
 
@@ -32,6 +51,7 @@ RSpec.describe KUtil::DataHelper do
         )
       end
     end
+
     context 'when complex hash' do
       let(:data) { { key1: 'David', key2: 333, items: [{ id: 1, name: 'Item1' }, { id: 2, name: 'Item2' }] } }
 
@@ -42,6 +62,53 @@ RSpec.describe KUtil::DataHelper do
           items: array_including(
             have_attributes(id: 1, name: 'Item1'),
             have_attributes(id: 2, name: 'Item2')
+          )
+        )
+      end
+    end
+
+    context 'when mixture of Struct and Hash at depth' do
+      let(:data) do
+        { key1: 'David',
+          key2: 333,
+          key3: ThunderBirds.new(:are_go),
+          items: [
+            { id: 1, action: ThunderBirds.new(:are_go) },
+            { id: 2, action: ThunderBirds.new(:are_grounded) }
+          ] }
+      end
+
+      it do
+        expect(subject).to have_attributes(
+          key1: 'David',
+          key2: 333,
+          key3: have_attributes(action: :are_go),
+          items: array_including(
+            have_attributes(id: 1, action: have_attributes(action: :are_go)),
+            have_attributes(id: 2, action: have_attributes(action: :are_grounded))
+          )
+        )
+      end
+    end
+
+    context 'when mixture of Struct, OpenStruct and Hash at depth' do
+      let(:p1) { OpenStruct.new(name: 'Virgil Tracy', age: 73, thunder_bird: ThunderBirds.new(:are_grounded)) }
+      let(:p2) { OpenStruct.new(name: 'Lady Penelope', age: 69, thunder_bird: ThunderBirds.new(:are_go)) }
+      let(:data) do
+        { key1: 'David',
+          key2: 333,
+          key3: ThunderBirds.new(:are_go),
+          people: [p1, p2] }
+      end
+
+      it do
+        expect(subject).to have_attributes(
+          key1: 'David',
+          key2: 333,
+          key3: have_attributes(action: :are_go),
+          people: array_including(
+            have_attributes(name: 'Virgil Tracy', age: 73, thunder_bird: have_attributes(action: :are_grounded)),
+            have_attributes(name: 'Lady Penelope', age: 69, thunder_bird: have_attributes(action: :are_go))
           )
         )
       end
